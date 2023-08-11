da7df4002a653e37ca91a0e5eef21bf4ce8c82a9
thayrov <thayrovg@gmail.com>
2023-08-11 10:21:30 -0600

    'Is n divisible by (...)?' and 'Filter the number' exercises

--- 7thKyu/20231208.js
+++ 7thKyu/20231208.js
@@ -18,6 +18,31 @@ console.log(filter_list([1, 2, 'aasf', '1', '123', 123])); // [1,2,123]
 
 //############################################################################
 
+//Is n divisible by (...)?
+
+//  The Problem
+/* Create a function that checks if the first argument n is divisible by all other arguments (return true if no other arguments)
+
+Example:
+
+(6,1,3)--> true because 6 is divisible by 1 and 3
+(12,2)--> true because 12 is divisible by 2
+(100,5,4,10,25,20)--> true
+(12,7)--> false because 12 is not divisible by 7 */
+
+//  The Solution
+
+const isDivisible = (x, ...y) => y.every(i => x % i === 0);
+
+//  The Test
+console.log(isDivisible(6, 1, 3)); // true (6 is divisible by both 1 and 3)
+console.log(isDivisible(12, 2)); // true (12 is divisible by 2)
+console.log(isDivisible(100, 5, 4, 10, 25, 20)); // true (100 is divisible by all the numbers provided)
+console.log(isDivisible(12, 7)); // false (12 is not divisible by 7)
+console.log(isDivisible(15)); // true (There are no divisors provided, so it should return true)
+
+//############################################################################
+
 //
 
 //  The Problem
