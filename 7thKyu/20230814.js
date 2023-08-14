574979e4991389d391517867d241cd7b85f3b58e
thayrov <thayrovg@gmail.com>
2023-08-14 09:24:56 -0600

    Disemvowel Trolls exercise

--- 7thKyu/20230814.js
+++ 7thKyu/20230814.js
@@ -17,5 +17,31 @@ const getCount = s => {
 };
 
 //  The Test
+/* console.log(getCount("example"));     // 3
+console.log(getCount("hello world")); // 3
+console.log(getCount("rhythm"));      // 0 */
+
+//############################################################################
+
+//Disemvowel Trolls
+
+//  The Problem
+/* Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel. */
+
+//  The Solution
+
+const disemvowel = s => s.replace(/[aeiouAEIOU]/g, '');
+
+//  The Test
+
+/* console.log(disemvowel("This website is for losers LOL!"));  // "Ths wbst s fr lsrs LL!" */
 
 //############################################################################
