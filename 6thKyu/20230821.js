bd0bc1c6669d8239705435c93803d7522fd0ae44
thayrov <thayrovg@gmail.com>
2023-08-21 17:59:07 -0600

    Find the odd int exercise

---
+++ 6thKyu/20230821.js
@@ -0,0 +1,40 @@
+//Find the odd int
+
+//  The Problem
+/* Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+ */
+//  The Solution
+/* const findOdd = arr =>
+	arr.find((item, index) => arr.filter(el => el == item).length % 2);
+ */
+const findOdd = arr => {
+	const occurrences = new Map();
+
+	arr.forEach(num => {
+		occurrences.set(num, (occurrences.get(num) || 0) + 1);
+	});
+
+	for (const [key, value] of occurrences.entries()) {
+		if (value % 2 !== 0) {
+			return key;
+		}
+	}
+};
+//  The Test
+/* console.log(findOdd([7]));                           // 7
+console.log(findOdd([0]));                           // 0
+console.log(findOdd([1,1,2]));                       // 2
+console.log(findOdd([0,1,0,1,0]));                   // 0
+console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]));   // 4
+ */
+//############################################################################
